541438c3779506909ec126f328cdbe0b056c1003
israellopez321 <israel.lopez@iesnervion.es>
2026-01-19 12:18:54 +0100

    Subir rama Israel

---
+++ Branch_Y_Merge/src/branchMerge/claseIsrael.java
@@ -0,0 +1,11 @@
+package branchMerge;
+
+public class claseIsrael {
+	public static void main(String[] args) {
+		System.out.println("Buenos dias, soy Israel");
+		int a;
+		a = 1+1;
+		System.out.println(a);
+		
+	}
+}
