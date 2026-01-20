70c7cab774dfb9cd0c9c88118fd01bdb6dc235a9
Miguel Perez <Miguel Perez@DESKTOP-GH138EU>
2026-01-20 19:56:36 +0100

    miguel

--- Branch_Y_Merge/src/branchMerge/BranchyMergeConComandos.java
+++ Branch_Y_Merge/src/branchMerge/BranchyMergeConComandos.java
@@ -2,7 +2,7 @@ package branchMerge;
 
 public class BranchyMergeConComandos {
 	public static void main(String[] args) {
-		System.out.println("Buenos dias");
-		System.out.println("Mundo");
+		System.out.println("Clase creada por Miguel");
+		
 	}
 }
