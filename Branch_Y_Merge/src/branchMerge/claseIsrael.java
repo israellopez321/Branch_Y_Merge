98f2e607da2a6ca3664675d86003cb26fa1a376e
Miguel Perez <Miguel Perez@DESKTOP-GH138EU>
2026-01-20 20:03:15 +0100

    cambio Miguel

--- Branch_Y_Merge/src/branchMerge/claseIsrael.java
+++ Branch_Y_Merge/src/branchMerge/claseIsrael.java
@@ -2,10 +2,11 @@ package branchMerge;
 
 public class claseIsrael {
 	public static void main(String[] args) {
-		System.out.println("Buenos dias, soy Israel");
-		int a;
-		a = 1+1;
-		System.out.println(a);
+		System.out.println("Buenos dias, soy Israel. Ahora Miguel");
+		int b=2;
+				b++;
+		System.out.println(b);
+		
 		
 	}
 }
