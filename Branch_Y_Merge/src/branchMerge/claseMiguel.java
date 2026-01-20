97c3b6bf2e712d4a1442bea56bd4fe417de88f6a
Miguel Perez <Miguel Perez@DESKTOP-GH138EU>
2026-01-20 20:13:14 +0100

    miguel

---
+++ Branch_Y_Merge/src/branchMerge/claseMiguel.java
@@ -0,0 +1,14 @@
+package branchMerge;
+
+public class claseMiguel {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("Clase de Miguel");
+		int b=2;
+		System.out.println(b);
+	}
+
+}
